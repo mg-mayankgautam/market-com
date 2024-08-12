df6a9a3708395d5cb0fe2fa3e91924ebeafc2331
Mansha <manshapreet02@gmail.com>
2024-08-12 15:04:51 +0530

    emailjs

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Contact.css'
 import location from '../../assets/locationlogo.png'
 import mail from '../../assets/maillogo.png'
+import emailjs from '@emailjs/browser';
 import call from '../../assets/calllogo.png'
 
 
 const Contact = ({contact_ref}) => {
+
+    useEffect(() => emailjs.init("tBVWuXxbt-lDrhJLU"), []);
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [msg, setMsg] = useState('')
+  
+    const sendEmail = async(e) => {
+        console.log(name, email, phone, msg)
+
+        try{
+            await emailjs.send('service_nom56r7', 'template_7nv92cr', {
+                name: name,
+                email: email,
+                phone: phone,
+                msg: msg
+            });
+            alert("email successfully sent to admin");
+        } 
+        catch (error) {
+            console.log(error);
+        }
+
+    }
+
+
   return (
 
     <div className='Contact' ref={contact_ref}>
@@ -18,11 +46,12 @@ const Contact = ({contact_ref}) => {
                     <br />or just say hello
                 </div>
 
-                <input type="text" placeholder='your name'/>
-                <input type="text" placeholder='your email'/>
-                <input type="text" placeholder='enter your message'/>
+                <input type="text" onChange={(e)=> setName(e.target.value)} placeholder='your name'/>
+                <input type="text" onChange={(e)=> setEmail(e.target.value)} placeholder='your email'/>
+                <input type="number" onChange={(e)=> setPhone(e.target.value)} placeholder='your phone'/>
+                <input type="text" onChange={(e)=> setMsg(e.target.value)} placeholder='enter your message'/>
 
-                <button className='contactBtn'>SEND</button>
+                <button className='contactBtn' onClick={sendEmail}>SEND</button>
             </div> 
         </div>
 
