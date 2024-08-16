a0d235add7072ed35b0422cb4d61f420b05e42d3
Mansha <manshapreet02@gmail.com>
2024-08-16 18:35:05 +0530

    bhurji page

--- src/components/ProjectPage/Bhurji.jsx
+++ src/components/ProjectPage/Bhurji.jsx
@@ -1,5 +1,21 @@
 import React, { useEffect } from 'react'
 import './ProjectPage.css'
+import logo from '../../assets/bhurji/logo.png'
+import logo1 from '../../assets/bhurji/logo1.png'
+import profile1 from '../../assets/bhurji/profile1.jpg'
+import profile2 from '../../assets/bhurji/profile2.jpg'
+import profile3 from '../../assets/bhurji/profile3.jpg'
+import profile4 from '../../assets/bhurji/profile4.jpg'
+import profile5 from '../../assets/bhurji/profile5.jpg'
+import profile6 from '../../assets/bhurji/profile6.jpg'
+import social1 from '../../assets/bhurji/social1.png'
+import social2 from '../../assets/bhurji/social2.png'
+import ui1 from '../../assets/bhurji/ui1.png'
+import ui2 from '../../assets/bhurji/ui2.png'
+import ui3 from '../../assets/bhurji/ui3.png'
+import ui4 from '../../assets/bhurji/ui4.png'
+import ui5 from '../../assets/bhurji/ui5.png'
+import ui6 from '../../assets/bhurji/ui6.png'
 
 const Bhurji = () => {
 
@@ -36,7 +52,7 @@ const Bhurji = () => {
                         About The Brand
                     </div>
                     <div className='proj_text'>
-
+                        Bhurji Electronics, established in 1972, is a leading transformer manufacturer in India. Initially holding a 75% market share in TV transformers, the company now develops advanced products to meet national energy needs. Pioneering C-Core Transformers, Bhurji Electronics has supplied constant voltage transformers to organizations like The Indian Railways, Ministry of Finance, Income Tax Department, HCL, WIPRO, and SIEMENS. Their Gurugram facility supports comprehensive manufacturing, including lamination cutting, punching, and developing transformer components for internal use.
                     </div>
 
                 </div>
@@ -64,11 +80,26 @@ const Bhurji = () => {
                         <div>Company Profile</div>
                     </div>
 
-                    <div className='companyProfile'>
+                    <div className='companyCatalogue'>
 
-                        {/* <div className='proj_halfImg'>
-                            <img src={p1} />
-                        </div> */}
+                        <div className='proj_halfImg'>
+                            <img src={profile1} loading='lazy'/>
+                        </div>
+                        <div className='proj_halfImg'>
+                            <img src={profile2} loading='lazy'/>
+                        </div>
+                        <div className='proj_halfImg'>
+                            <img src={profile3} loading='lazy'/>
+                        </div>
+                        <div className='proj_halfImg'>
+                            <img src={profile4} loading='lazy'/>
+                        </div>
+                        <div className='proj_halfImg'>
+                            <img src={profile5} loading='lazy'/>
+                        </div>
+                        <div className='proj_halfImg'>
+                            <img src={profile6} loading='lazy'/>
+                        </div>
 
                     </div>
                 </div>
@@ -80,7 +111,12 @@ const Bhurji = () => {
                     </div>
 
                     <div className='companyLogos'>
-
+                        <div className='proj_halfImg'>
+                            <img src={logo} loading='lazy'/>
+                        </div>
+                        <div className='proj_halfImg'>
+                            <img src={logo1} loading='lazy'/>
+                        </div>
                     </div>
 
                 </div>
@@ -88,11 +124,16 @@ const Bhurji = () => {
                 <div className='proj_Section'>
                     <div className='proj_SubHead'>
                         <div className='proj_numHead'>3</div>
-                        <div>Info Graphics Design</div>
+                        <div>Social Media Posts</div>
                     </div>
 
-                    <div className='companyInfoGr'>
-
+                    <div className='companyProfile'>
+                        <div className='proj_halfImg'>
+                            <img src={social1} loading='lazy'/>
+                        </div>
+                        <div className='proj_halfImg'>
+                            <img src={social2} loading='lazy'/>
+                        </div>
                     </div>
 
                 </div>
@@ -104,7 +145,24 @@ const Bhurji = () => {
                     </div>
 
                     <div className='companyUI'>
-
+                        <div className='proj_halfImg'>
+                            <img src={ui1} loading='lazy'/>
+                        </div>
+                        <div className='proj_halfImg'>
+                            <img src={ui2} loading='lazy'/>
+                        </div>
+                        <div className='proj_halfImg'>
+                            <img src={ui3} loading='lazy'/>
+                        </div>
+                        <div className='proj_halfImg'>
+                            <img src={ui4} loading='lazy'/>
+                        </div>
+                        <div className='proj_halfImg'>
+                            <img src={ui5} loading='lazy'/>
+                        </div>
+                        <div className='proj_halfImg'>
+                            <img src={ui6} loading='lazy'/>
+                        </div>
                     </div>
 
                 </div>
