8e10c9133f09ca632177c8f08c90e50f3ba2b898
Mansha <manshapreet02@gmail.com>
2024-08-23 21:30:22 +0530

    link btn

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -13,8 +13,8 @@ const Services = () => {
 
     const [showMoreServices, setshowMoreServices] = useState(false)
 
-    const showMoreservices = ()=>{
-        setshowMoreServices(prev=>!prev)
+    const showMoreservices = () => {
+        setshowMoreServices(prev => !prev)
     }
 
     const [first, setfirst] = useState(true);
@@ -23,247 +23,249 @@ const Services = () => {
     const [fourth, setfourth] = useState(false);
     const [fifth, setfifth] = useState(false);
 
-    const toggle1 = () =>{
-        if(!first)setfirst(e=>!e)
+    const toggle1 = () => {
+        if (!first) setfirst(e => !e)
 
-        if(second)setsecond(e=>!e)
-        if(third)setthird(e=>!e)
-        if(fourth)setfourth(e=>!e)
-        if(fifth)setfifth(e=>!e)        
-
-    }
-    const toggle2 = () =>{
-        if(!second)setsecond(e=>!e)
-
-        if(first)setfirst(e=>!e)
-        if(third)setthird(e=>!e)
-        if(fourth)setfourth(e=>!e)
-        if(fifth)setfifth(e=>!e)    
+        if (second) setsecond(e => !e)
+        if (third) setthird(e => !e)
+        if (fourth) setfourth(e => !e)
+        if (fifth) setfifth(e => !e)
 
     }
-    const toggle3 = () =>{
-        if(!third)setthird(e=>!e)
+    const toggle2 = () => {
+        if (!second) setsecond(e => !e)
 
-        if(first)setfirst(e=>!e)
-        if(second)setsecond(e=>!e)
-        if(fourth)setfourth(e=>!e)
-        if(fifth)setfifth(e=>!e)    
+        if (first) setfirst(e => !e)
+        if (third) setthird(e => !e)
+        if (fourth) setfourth(e => !e)
+        if (fifth) setfifth(e => !e)
+
+    }
+    const toggle3 = () => {
+        if (!third) setthird(e => !e)
+
+        if (first) setfirst(e => !e)
+        if (second) setsecond(e => !e)
+        if (fourth) setfourth(e => !e)
+        if (fifth) setfifth(e => !e)
 
 
 
     }
-    const toggle4 = () =>{
-        if(!fourth)setfourth(e=>!e)
+    const toggle4 = () => {
+        if (!fourth) setfourth(e => !e)
 
 
-        if(first)setfirst(e=>!e)
-        if(second)setsecond(e=>!e)
-        if(third)setthird(e=>!e)             
-        if(fifth)setfifth(e=>!e)    
-
-    }
-    const toggle5 = () =>{
-      if(!fifth)setfifth(e=>!e)
-
-      if(first)setfirst(e=>!e)
-      if(second)setsecond(e=>!e)
-      if(third)setthird(e=>!e)             
-      if(fourth)setfourth(e=>!e)  
-        
+        if (first) setfirst(e => !e)
+        if (second) setsecond(e => !e)
+        if (third) setthird(e => !e)
+        if (fifth) setfifth(e => !e)
 
     }
+    const toggle5 = () => {
+        if (!fifth) setfifth(e => !e)
 
-  return (
-    <div className='Services'>
-        
-        <div className='servicesHead'>OUR SERVICES</div>
+        if (first) setfirst(e => !e)
+        if (second) setsecond(e => !e)
+        if (third) setthird(e => !e)
+        if (fourth) setfourth(e => !e)
 
-        <div className='ServicesContainer'>
 
-          <div className='serviceBox'>
-            <div className='serviceImgDiv'>
-                <img src={webdev} loading='lazy'/>
-            </div>
-            <div className='serviceContent'>
-                <div className='serviceName'>
-                    Website Development
+    }
+
+    return (
+        <div className='Services'>
+
+            <div className='servicesHead'>OUR SERVICES</div>
+
+            <div className='ServicesContainer'>
+
+                <div className='serviceBox'>
+                    <div className='serviceImgDiv'>
+                        <img src={webdev} loading='lazy' />
+                    </div>
+                    <div className='serviceContent'>
+                        <div className='serviceName'>
+                            Website Development
+                        </div>
+                        <Link to="service/websitedevelopement">
+                            <button className='serviceBtn'>
+                                Read More
+                            </button>
+                        </Link>
+                    </div>
                 </div>
-                <button className='serviceBtn'>
-                <Link to="service/websitedevelopement">Read More</Link>
+
+                <div className='serviceBox'>
+                    <div className='serviceImgDiv'>
+                        <img src={appdev} loading='lazy' />
+                    </div>
+                    <div className='serviceContent'>
+                        <div className='serviceName'>
+                            App Development
+                        </div>
+                        <button className='serviceBtn'>
+                            Read More
+                        </button>
+                    </div>
+                </div>
+
+                <div className='serviceBox'>
+                    <div className='serviceImgDiv'>
+                        <img src={pdesign} loading='lazy' />
+                    </div>
+                    <div className='serviceContent'>
+                        <div className='serviceName'>
+                            Product Design
+                        </div>
+                        <button className='serviceBtn'>
+                            Read More
+                        </button>
+                    </div>
+                </div>
+
+                <div className='serviceBox'>
+                    <div className='serviceImgDiv'>
+                        <img src={sm} loading='lazy' />
+                    </div>
+                    <div className='serviceContent'>
+                        <div className='serviceName'>
+                            Social Media Marketing
+                        </div>
+                        <Link to="/service/socialmedia">
+                            <button className='serviceBtn'>
+                                Read More
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className='serviceBox'>
+                    <div className='serviceImgDiv'>
+                        <img src={cm} loading='lazy' />
+                    </div>
+                    <div className='serviceContent'>
+                        <div className='serviceName'>
+                            Corporate Strategy
+                        </div>
+                        <button className='serviceBtn'>
+                            Read More
+                        </button>
+                    </div>
+                </div>
+
+                <div className='serviceBox'>
+                    <div className='serviceImgDiv'>
+                        <img src={seo} loading='lazy' />
+                    </div>
+                    <div className='serviceContent'>
+                        <div className='serviceName'>
+                            Search Engine Optimization
+                        </div>
+                        <button className='serviceBtn'>
+                            Read More
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+
+            {showMoreServices ? <></> :
+                <button className='viewMoreBtn'
+                    onClick={() => { showMoreservices() }}
+                >
+                    View More services
                 </button>
-               
-            </div>
-          </div> 
+            }
 
-          <div className='serviceBox'>
-            <div className='serviceImgDiv'>
-                <img src={appdev} loading='lazy'/>
-            </div>
-            <div className='serviceContent'>
-                <div className='serviceName'>
-                    App Development
-                </div>
-                <button className='serviceBtn'>
-                    Read More
-                </button>
-            </div>
-          </div> 
+            {showMoreServices ?
+                <div className='more_service_container'>
 
-          <div className='serviceBox'>
-            <div className='serviceImgDiv'>
-                <img src={pdesign} loading='lazy'/>
-            </div>
-            <div className='serviceContent'>
-                <div className='serviceName'>
-                    Product Design
-                </div>
-                <button className='serviceBtn'>
-                    Read More
-                </button>
-            </div>
-          </div> 
+                    <div className='headings'>
+                        <div className={first ? 'service_heading selected' : 'service_heading'} onClick={() => { toggle1() }}>DESIGN</div>
+                        <div className={second ? 'service_heading selected' : 'service_heading'} onClick={() => { toggle2() }}>DIGITAL MARKETING</div>
+                        <div className={third ? 'service_heading selected' : 'service_heading'} onClick={() => { toggle3() }}>DEVELOPEMENT</div>
+                        <div className={fourth ? 'service_heading selected' : 'service_heading'} onClick={() => { toggle4() }}>SEO</div>
+                        <div className={fifth ? 'service_heading selected' : 'service_heading'} onClick={() => { toggle5() }}>CONTENT WRITING</div>
+                    </div>
 
-          <div className='serviceBox'>
-            <div className='serviceImgDiv'>
-                <img src={sm} loading='lazy'/>
-            </div>
-            <div className='serviceContent'>
-                <div className='serviceName'>
-                    Social Media Marketing
-                </div>
-                <button className='serviceBtn'>
-                    <Link to="/service/socialmedia">Read More</Link>
-                    
-                </button>
-            </div>
-          </div> 
+                    <div className='content'>
+                        {first ?
+                            <div >
+                                <div className='service'>UI/UX DESIGN</div>
+                                <div className='service'>E-COMMERCE DESIGN</div>
+                                <div className='service'>APP DESIGN</div>
+                                <div className='service'>PRODUCT DESIGN</div>
+                                <div className='service'>LOGO DESIGN</div>
+                                <div className='service'>CARD DESIGN</div>
+                                <div className='service'>SOCIAL MEDIA POST DESIGN</div>
+                                <div className='service'>BANNER DESIGN</div>
+                                <div className='service'>ADVERTISMENT DESIGN</div>
+                                <div className='service'>CATALOGUE DESIGN</div>
 
-          <div className='serviceBox'>
-            <div className='serviceImgDiv'>
-                <img src={cm} loading='lazy'/>
-            </div>
-            <div className='serviceContent'>
-                <div className='serviceName'>
-                    Corporate Strategy
-                </div>
-                <button className='serviceBtn'>
-                    Read More
-                </button>
-            </div>
-          </div> 
-
-          <div className='serviceBox'>
-            <div className='serviceImgDiv'>
-                <img src={seo} loading='lazy'/>
-            </div>
-            <div className='serviceContent'>
-                <div className='serviceName'>
-                    Search Engine Optimization
-                </div>
-                <button className='serviceBtn'>
-                    Read More
-                </button>
-            </div>
-          </div> 
-
-        </div>
-
-        {showMoreServices?<></>:
-            <button className='viewMoreBtn'
-                onClick={()=>{showMoreservices()}}
-            >
-                View More services
-            </button>
-        }
-
-        {showMoreServices?
-            <div className='more_service_container'>
-
-                <div className='headings'>
-                    <div className={first? 'service_heading selected':'service_heading'} onClick={()=>{toggle1()}}>DESIGN</div>
-                    <div className={second? 'service_heading selected':'service_heading'} onClick={()=>{toggle2()}}>DIGITAL MARKETING</div>
-                    <div className={third? 'service_heading selected':'service_heading'} onClick={()=>{toggle3()}}>DEVELOPEMENT</div>
-                    <div className={fourth? 'service_heading selected':'service_heading'} onClick={()=>{toggle4()}}>SEO</div>
-                    <div className={fifth? 'service_heading selected':'service_heading'} onClick={()=>{toggle5()}}>CONTENT WRITING</div>
-                </div>
-
-                <div className='content'>
-                    {first? 
-                    <div >
-                        <div className='service'>UI/UX DESIGN</div>
-                        <div className='service'>E-COMMERCE DESIGN</div>
-                        <div className='service'>APP DESIGN</div>
-                        <div className='service'>PRODUCT DESIGN</div>
-                        <div className='service'>LOGO DESIGN</div>
-                        <div className='service'>CARD DESIGN</div>
-                        <div className='service'>SOCIAL MEDIA POST DESIGN</div>
-                        <div className='service'>BANNER DESIGN</div>
-                        <div className='service'>ADVERTISMENT DESIGN</div>
-                        <div className='service'>CATALOGUE DESIGN</div>
-                        
-                        <div className='service'>Infographics Design</div>
+                                <div className='service'>Infographics Design</div>
 
 
 
-                    </div>:<></>}
+                            </div> : <></>}
 
-                    {second?
-                    <div >
-                        <div className='service'>FB MARKETING</div>
-                        <div className='service'>INSTA MARKETING</div>
-                        <div className='service'>X MARKETING</div>
-                        <div className='service'>LINKEDIN MARKETING</div>
-                        <div className='service'>AFFILIATE MARKETING</div>
-                        <div className='service'>EMAIL MARKETING</div>
-                        <div className='service'>VIDEO MARKETING</div>
-                        <div className='service'>VIDEO EDITING</div>
-                        <div className='service'>INSTA REEL EDITING</div>
-                        <div className='service'>YOUTUBE SHORTS EDITING</div>
+                        {second ?
+                            <div >
+                                <div className='service'>FB MARKETING</div>
+                                <div className='service'>INSTA MARKETING</div>
+                                <div className='service'>X MARKETING</div>
+                                <div className='service'>LINKEDIN MARKETING</div>
+                                <div className='service'>AFFILIATE MARKETING</div>
+                                <div className='service'>EMAIL MARKETING</div>
+                                <div className='service'>VIDEO MARKETING</div>
+                                <div className='service'>VIDEO EDITING</div>
+                                <div className='service'>INSTA REEL EDITING</div>
+                                <div className='service'>YOUTUBE SHORTS EDITING</div>
 
-                    </div>:<></>}
+                            </div> : <></>}
 
 
-                    {third? <div >
-                        <div className='service'>CUSTOM WEBSITES</div>
-                        <div className='service'>STATIC WEBSITES</div>
-                        <div className='service'>PRODUCT WEBSITES</div>
-                        <div className='service'>SHOPIFY DEVELOPEMENT</div>
-                        <div className='service'>WORDPRESS DEVELOPMENT</div>
-                        <div className='service'>IOS DEVELOPMENT</div>
-                        <div className='service'>ANDROID DEVELOPMENT</div>
-                        <div className='service'>ECOMMERCE WEBSITES</div>
-                        <div className='service'>CMS(CONTENT Management SYSTEM)</div>
-                        <div className='service'>YOUTUBE SHORTS EDITING</div>
-                    </div>:<></>}
+                        {third ? <div >
+                            <div className='service'>CUSTOM WEBSITES</div>
+                            <div className='service'>STATIC WEBSITES</div>
+                            <div className='service'>PRODUCT WEBSITES</div>
+                            <div className='service'>SHOPIFY DEVELOPEMENT</div>
+                            <div className='service'>WORDPRESS DEVELOPMENT</div>
+                            <div className='service'>IOS DEVELOPMENT</div>
+                            <div className='service'>ANDROID DEVELOPMENT</div>
+                            <div className='service'>ECOMMERCE WEBSITES</div>
+                            <div className='service'>CMS(CONTENT Management SYSTEM)</div>
+                            <div className='service'>YOUTUBE SHORTS EDITING</div>
+                        </div> : <></>}
 
-                    {fourth?<div >
-                        <div className='service'>ON-PAGE SEO</div>
-                        <div className='service'>OFF PAGE SEO</div>
-                        <div className='service'>ECOMMERCE SEO</div>
-                        <div className='service'>MULTILNGUAL SEO</div>
-                        <div className='service'>ENTERPRISE SEO</div>
-                        <div className='service'>YOUTUBE SEO</div>
-                        <div className='service'>APP STORE OPTIMSATION</div>
-                        {/* <div>ANDROID DEVELOPMENT</div>
+                        {fourth ? <div >
+                            <div className='service'>ON-PAGE SEO</div>
+                            <div className='service'>OFF PAGE SEO</div>
+                            <div className='service'>ECOMMERCE SEO</div>
+                            <div className='service'>MULTILNGUAL SEO</div>
+                            <div className='service'>ENTERPRISE SEO</div>
+                            <div className='service'>YOUTUBE SEO</div>
+                            <div className='service'>APP STORE OPTIMSATION</div>
+                            {/* <div>ANDROID DEVELOPMENT</div>
                         
                         <div>CMS(CONTENT Management SYSTEM)</div>
                         <div>YOUTUBE SHORTS EDITING</div> */}
-                    </div>:<></>}
-                    {fifth? <div > <div className='service'>WEBSITE CONTENT WRITING</div></div>:<></>}
-                    
-                   
-                    
+                        </div> : <></>}
+                        {fifth ? <div > <div className='service'>WEBSITE CONTENT WRITING</div></div> : <></>}
+
+
+
+                    </div>
+
+
                 </div>
-                
+                : <></>}
 
-            </div>
-        :<></>}
 
-        
 
-        
 
-        {/* <div>Search Engine Optimization</div>
+
+            {/* <div>Search Engine Optimization</div>
         <div>Digital Marketing</div>
         <div>Product Design</div>
         <div>Package Design</div>
@@ -285,10 +287,10 @@ const Services = () => {
 
 
 
-    </div>
-    
+        </div>
 
-  )
+
+    )
 }
 
 export default Services
