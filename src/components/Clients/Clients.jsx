6b1fa0ea81494e5a77e51c882271954b55980ab4
Mansha <manshapreet02@gmail.com>
2024-08-25 14:59:18 +0530

    project hover

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -18,6 +18,12 @@ import { Link } from 'react-router-dom';
 
 const Clients = () => {
 
+  const [style1, setStyle1] = useState({display:'none'})
+  const [style2, setStyle2] = useState({display:'none'})
+  const [style3, setStyle3] = useState({display:'none'})
+  const [style4, setStyle4] = useState({display:'none'})
+  const [style5, setStyle5] = useState({display:'none'})
+
   return (
 
     <div className='Clients'>
@@ -55,10 +61,16 @@ const Clients = () => {
 
         <SwiperSlide>
           <Link to='/project/covendx'>
-            <div className='projectCard'>
-              <img src={covendx} alt="" loading='lazy'/>
+            <div className='projectCard'
+              onMouseEnter={e => { setStyle1({ display: 'flex' }) }}
+              onMouseLeave={e => { setStyle1({ display: 'none' }) }}
+              >
+              <img src={covendx} alt="" loading='lazy' />
               <div className='projName'>
                 Covendx
+              </div>
+              <div className='projecthover' style={style1}>
+                <div className='hoverbtn'>Read More</div>
               </div>
             </div>
           </Link>
@@ -66,10 +78,16 @@ const Clients = () => {
 
         <SwiperSlide>
           <Link to='/project/moheera'>
-            <div className='projectCard'>
-              <img src={moheera} alt="" loading='lazy'/>
+            <div className='projectCard'
+              onMouseEnter={e => { setStyle2({ display: 'flex' }) }}
+              onMouseLeave={e => { setStyle2({ display: 'none' }) }}
+              >
+              <img src={moheera} alt="" loading='lazy' />
               <div className='projName'>
                 Moheera Jewels
+              </div>
+              <div className='projecthover' style={style2}>
+                <div className='hoverbtn'>Read More</div>
               </div>
             </div>
           </Link>
@@ -77,10 +95,16 @@ const Clients = () => {
 
         <SwiperSlide>
           <Link to='/project/bhurjielectronics'>
-            <div className='projectCard'>
-              <img src={bhurji} alt="" loading='lazy'/>
+            <div className='projectCard'
+              onMouseEnter={e => { setStyle3({ display: 'flex' }) }}
+              onMouseLeave={e => { setStyle3({ display: 'none' }) }}
+              >
+              <img src={bhurji} alt="" loading='lazy' />
               <div className='projName'>
                 Bhurji Electronics
+              </div>
+              <div className='projecthover' style={style3}>
+                <div className='hoverbtn'>Read More</div>
               </div>
             </div>
           </Link>
@@ -89,7 +113,7 @@ const Clients = () => {
         <SwiperSlide>
           <Link>
             <div className='projectCard'>
-              <img src={businesscard} alt="" style={{objectPosition:'center right'}} loading='lazy'/>
+              <img src={businesscard} alt="" style={{ objectPosition: 'center right' }} loading='lazy' />
               <div className='projName'>
                 Business Card UI
               </div>
@@ -100,7 +124,7 @@ const Clients = () => {
         <SwiperSlide>
           <Link to='https://www.thefuelmedia.co/' target='_blank'>
             <div className='projectCard'>
-              <img src={fuelmedia} alt="" loading='lazy'/>
+              <img src={fuelmedia} alt="" loading='lazy' />
               <div className='projName'>
                 The Fuel Media
               </div>
@@ -110,10 +134,16 @@ const Clients = () => {
 
         <SwiperSlide>
           <Link to='/project/aadmiwala'>
-            <div className='projectCard'>
-              <img src={aadmiwala} alt="" loading='lazy'/>
+            <div className='projectCard'
+              onMouseEnter={e => { setStyle4({ display: 'flex' }) }}
+              onMouseLeave={e => { setStyle4({ display: 'none' }) }}
+              >
+              <img src={aadmiwala} alt="" loading='lazy' />
               <div className='projName'>
                 Aadmiwala
+              </div>
+              <div className='projecthover' style={style4}>
+                <div className='hoverbtn'>Read More</div>
               </div>
             </div>
           </Link>
@@ -121,10 +151,16 @@ const Clients = () => {
 
         <SwiperSlide>
           <Link to='/project/homeproject'>
-            <div className='projectCard'>
-              <img src={homeproj} alt="" loading='lazy'/>
+            <div className='projectCard'
+              onMouseEnter={e => { setStyle5({ display: 'flex' }) }}
+              onMouseLeave={e => { setStyle5({ display: 'none' }) }}
+              >
+              <img src={homeproj} alt="" loading='lazy' />
               <div className='projName'>
                 Home Project
+              </div>
+              <div className='projecthover' style={style5}>
+                <div className='hoverbtn'>Read More</div>
               </div>
             </div>
           </Link>
@@ -133,7 +169,7 @@ const Clients = () => {
         <SwiperSlide>
           <Link to='https://ruralhandmade.com/' target='_blank'>
             <div className='projectCard'>
-              <img src={ruralhand} alt="" loading='lazy'/>
+              <img src={ruralhand} alt="" loading='lazy' />
               <div className='projName'>
                 Rural Handmade
               </div>
