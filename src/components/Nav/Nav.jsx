f6c171a1b4d79f1061e025ed5368fb652fc3500e
Mansha <manshapreet02@gmail.com>
2024-09-07 20:30:27 +0530

    nav works on all pgs

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,29 +1,63 @@
-import React from 'react'
-import './Nav.css'
-import logo from '../../assets/logo.png'
-import { Link } from 'react-router-dom'
-import { useRef } from 'react'
+import React, { useRef, useState, useEffect } from 'react';
+import './Nav.css';
+import logo from '../../assets/logo.png';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const Nav = ({ about_ref, contact_ref }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [scrollToAbout, setScrollToAbout] = useState(false);
+  const [scrollToContact, setScrollToContact] = useState(false);
+
+  useEffect(() => {
+    if (scrollToAbout && about_ref.current) {
+      about_ref.current.scrollIntoView({ behavior: 'smooth' });
+      setScrollToAbout(false); // Reset the flag after scrolling
+    }
+  }, [scrollToAbout, about_ref]);
+
+  useEffect(() => {
+    if (scrollToContact && contact_ref.current) {
+      contact_ref.current.scrollIntoView({ behavior: 'smooth' });
+      setScrollToContact(false); // Reset the flag after scrolling
+    }
+  }, [scrollToContact, contact_ref]);
+
+  const navigateAbout = () => {
+    if (location.pathname === '/') {
+      setScrollToAbout(true); // Set the flag to true if already on the homepage
+    } else {
+      navigate('/'); // Navigate to home
+      setScrollToAbout(true); // Set the flag to true to scroll after navigating
+    }
+  };
+
+  const navigateContact = () => {
+    if (location.pathname === '/') {
+      setScrollToContact(true); // Set the flag to true if already on the homepage
+    } else {
+      navigate('/'); // Navigate to home
+      setScrollToContact(true); // Set the flag to true to scroll after navigating
+    }
+  };
 
   return (
     <div className='Nav'>
       <div>
-        <Link to={'/'} className='navlogo'>
-          <img src={logo} className='logo' />
+        <Link to='/' className='navlogo'>
+          <img src={logo} className='logo' alt='Logo' />
         </Link>
       </div>
 
       <div className='navItems'>
-      <Link to={'/'} >
-        <div ><button className='nav_btn'>Home</button></div>
-      </Link>
-       
+        <Link to='/'>
+          <div><button className='nav_btn'>Home</button></div>
+        </Link>
 
         <div>
           <button
             className='nav_btn'
-            onClick={() => { about_ref.current?.scrollIntoView({ behavior: 'smooth' }) }}
+            onClick={navigateAbout}
           >
             About
           </button>
@@ -32,14 +66,14 @@ const Nav = ({ about_ref, contact_ref }) => {
         <div>
           <button
             className='nav_btn'
-            onClick={() => { contact_ref.current?.scrollIntoView({ behavior: 'smooth' }) }}
+            onClick={navigateContact}
           >
             Contact
           </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
